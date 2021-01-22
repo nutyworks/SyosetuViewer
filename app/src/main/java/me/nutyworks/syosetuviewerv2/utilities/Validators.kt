7067064a3e3f5eca002f8edf0bc2642df539b39d
nutyworks <nutyworks@gmail.com>
2021-01-22 12:00:15 +0900

    Update ncode validator to accept capitalized ncodes

--- app/src/main/java/me/nutyworks/syosetuviewerv2/utilities/Validators.kt
+++ app/src/main/java/me/nutyworks/syosetuviewerv2/utilities/Validators.kt
@@ -1,7 +1,7 @@
 package me.nutyworks.syosetuviewerv2.utilities
 
 object NcodeValidator {
-    private val regex = """n\d{4}[a-z]{1,2}""".toRegex()
+    private val regex = """[Nn]\d{4}[A-Za-z]{1,2}""".toRegex()
     fun validate(ncode: String) {
         if (!ncode.matches(regex)) throw ValidatorException("Ncode does not match with regex $regex")
     }
