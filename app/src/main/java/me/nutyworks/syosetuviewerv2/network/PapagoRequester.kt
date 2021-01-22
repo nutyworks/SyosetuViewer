af1f403aa5c890080ba62998edd423a498febea7
nutyworks <nutyworks@gmail.com>
2021-01-22 23:06:03 +0900

    Remove wildcard imports

--- app/src/main/java/me/nutyworks/syosetuviewerv2/network/PapagoRequester.kt
+++ app/src/main/java/me/nutyworks/syosetuviewerv2/network/PapagoRequester.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
-import java.util.*
+import java.util.UUID
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
