d1bb8ff0a349dc2307d2e498d8efd7fcf36b7484
nutyworks <nutyworks@gmail.com>
2021-01-23 19:57:19 +0900

    Change bottom navigation menu icon and name

--- app/src/main/java/me/nutyworks/syosetuviewerv2/MainActivity.kt
+++ app/src/main/java/me/nutyworks/syosetuviewerv2/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_novel_list,
-                R.id.navigation_dashboard,
+                R.id.navigation_search,
                 R.id.navigation_notifications
             )
         )
